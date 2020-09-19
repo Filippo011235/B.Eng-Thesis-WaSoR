7b9962c90434dca58df0985d0e5a1e6f6bb8f13b
Filippo011235 <fadamcewicz1@gmail.com>
2020-09-20 00:46:23 +0200

    Preparation of test data and 246 - 258

--- 1st experiment backup/ImageProcessing.py
+++ 1st experiment backup/ImageProcessing.py
@@ -6,7 +6,8 @@ DirNames = ("a01/", "a02/", "a05/", "a06/")
 DirRaw = "./Raw from Wadaba/"
 DirPostProcessing = "./PostProcessing Data/"
 PostProcSize = (120,120) # pixels
-#MaterialRatio = (16, 55, 52, 60)
+MaterialRatio = (16, 55, 52, 60) # How many more images have to be created from original dataset
+# Rotation angle needed to achieve MatRatio. Each pic will be rotated by this angle
 MaterialAngle = (360/MaterialRatio[0], 360/MaterialRatio[1], 360/MaterialRatio[2], 360/MaterialRatio[3])
 
 def ProcessImage(CurrentImage, j, RotAngle):
@@ -14,19 +15,22 @@ def ProcessImage(CurrentImage, j, RotAngle):
     
 if __name__ == "__main__":
 
+    input("Press Enter to begin")
+
     for Material in range(NoOfMaterials):
+        print("Material no.: ", Material)
         CurrentMatDir = DirRaw + DirNames[Material]
         RawImgListing = listdir(CurrentMatDir)
 
-        boundary = 0
-
         for ImgName in RawImgListing:
-            boundary = boundary + 1
+            print("Working on: ", ImgName)
             CurrentImg = Image.open(CurrentMatDir + ImgName)
             RotAngle = MaterialAngle[Material]
             for j in range(MaterialRatio[Material]):
                 PostProcImg = ProcessImage(CurrentImg, j, RotAngle)
-                PostProcImg.save(DirPostProcessing + DirNames[Material] + str(j) + ImgName)
+                # Create new name for image -> appendix rXX.jpg where XX is "j" from loop
+                NewImgName = ImgName[:-4] + "r" + str(j) + ".jpg"
+                PostProcImg.save(DirPostProcessing + DirNames[Material] + NewImgName)
             CurrentImg.close()
-            if boundary >= 3:
-                break 
+
+    input("Press Enter to continue...")
