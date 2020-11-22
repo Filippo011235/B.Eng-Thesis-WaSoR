25e9cdffded7d5feac607af407e40b0c50154621
Filippo011235 <fadamcewicz1@gmail.com>
2020-11-22 15:50:21 +0100

    314 - 327
    PostFinal new waste

--- Dataset/NamingScript.py
+++ Dataset/NamingScript.py
@@ -6,17 +6,18 @@ ObjectIdx = int(input("Idx of the 1st object: "))
 
 i = 0 
 while i < len(OriginalNames):
-    NoOfImages = input("No. of object images: ")
     CodeName = input("Code-name: ")
+    NoOfImages = input("No. of object images: ")
     print("00 unknown | 01 PET | 02 PEHD | 03 PVC | 04 PELD | 05 PP | 06 PS | 07 Other")
     TypeOfPlastic = input("Type of plastic(only 2nd number): 0")
-    j = 0
+    
     # Copy first Img of Waste to "./List of each waste" dir, with CodeName
-    RepresentiveImg = '0'+str(ObjectIdx) + '_a0'+TypeOfPlastic + 'h'+str(j) + "_"+CodeName + ".JPG"  
-    copy2('./iCloud Photos/'+OriginalNames[i], './List of each waste/'+RepresentiveImg)
+    RepresentiveImg = '0' + str(ObjectIdx) + '_a0' + TypeOfPlastic + 'h0_' + CodeName + ".jpg" 
+    copy2('./iCloud Photos/'+OriginalNames[i], './List of each waste/' + RepresentiveImg)
+    
     # Rename all Images of Waste, in the current dir
     for j in range(int(NoOfImages)):
-        Designation = '0'+str(ObjectIdx) + '_a0'+TypeOfPlastic + 'h'+str(j) + ".JPG"  
+        Designation = '0' + str(ObjectIdx) + '_a0' + TypeOfPlastic + 'h' + str(j) + ".JPG"  
         print(OriginalNames[i], " to ", Designation)
         rename('./iCloud Photos/'+OriginalNames[i], './iCloud Photos/'+Designation)
         i += 1
